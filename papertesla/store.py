1b96bdf48c0dfa2c6e71b15bf72ea048b08b25d4
Braden Mars <bradenmars@bradenmars.me>
2020-02-06 17:16:54 -0600

    feat: added orders endpoint with price calculation

--- papertesla/store.py
+++ papertesla/store.py
@@ -5,9 +5,22 @@
 Paper Tesla Serverless Api Store Handler
 """
 
+import uuid
+
 import simplejson as json
 
 from papertesla import data
+
+
+def find_product(model, size):
+    size_filter = ("size", size)
+    products = data.DynamoDB('products')
+    product = iter(products.query(
+        model,
+        key="model",
+        index="model-index",
+        filters=size_filter))
+    return next(product, None)
 
 
 def get_products(event, context):
@@ -19,11 +32,45 @@ def get_products(event, context):
     return resp
 
 
-def handler(event, context):
-    print("Lambda called!\n")
-    print(event)
-    print("\n")
-    print("Context:")
-    print(context.__dict__)
+def create_order(event, context):
+    req = json.loads(event.get('body'))
+    order = req.get('order')
 
-    return None
+    options = order['options']
+    opts_complete = options.copy()
+    size = options.pop('size')
+
+    product = find_product(order['model'], size)
+
+    # grab prices for each selected addon
+    addons = [k for k, v in options.items() if v]
+    prices = [v for k, v in product['pricing'].items() if k in addons]
+    # append base price
+    prices.append(product['pricing']['base'])
+    # add it all up
+    total_price = sum(prices)
+
+    # unverified order request
+    order_obj = {
+        'id': str(uuid.uuid4()),
+        'email': 'example@example.com',
+        'completed': False,
+        'order': {
+            'productId': product['id'],
+            'options': opts_complete,
+            'pricing': {
+                'retail': total_price
+            }
+        }
+    }
+
+    # add it to table
+    orders = data.DynamoDB('orders')
+    orders.add_item(order_obj)
+
+    # respond
+    resp = {
+        'statusCode': 200,
+        'body': json.dumps(order_obj)
+    }
+    return resp
